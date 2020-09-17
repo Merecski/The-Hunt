c37d519bd53cca7c8a27ef2ea4d60feb2a1fa5d0
Merecski <merecski@Borealis.localdomain>
2020-09-17 11:36:20 -0400

    Add foundation for AI/User decision logic
    Added fucntions for both AI and User decisions.
    
    Created functions checkBoundry, distance, calcluate, appendHistory
    to easily manipulate positional data and decision making.
    
    Current AI logic checks all directions for the highest
    distance from player
    
    Placing the players whill shuffle position until they are at
    least a third of the board away from eachother.
    
    Fixed indexing bug with loading level settings

--- bin/thehunt.py
+++ bin/thehunt.py
@@ -2,20 +2,93 @@
 
 import curses
 import json
+import math
+import random
 from time import sleep
 
-LEVELSFILE = 'levels.json'
+random.seed()
+LEVELSFILE = 'data/levels.json'
+
 
 class TheHunt:
     def __init__(self):
+        self.axis = [0, 0]
+        self.grid = False
+        self.gun_enable = False
+        self.gun_ammo = None
+        self.gun_prob = None
         self.hist = {'ai':[], 'usr':[]}
         self.pos = {'ai':[0, 0], 'usr':[0, 0]}
+        self.quit = False
         self.window = None
+
+        self.translate = {
+            'w':[-1,  0],
+            'a':[ 0, -1],
+            's':[ 1,  0],
+            'd':[ 0,  1],
+            ' ':[ 0,  0]
+        }
 
     def action(self, player, direction):
         return {'move':direction, 'loc':self.pos[player]}
 
+    def appendHistory(self, player, move):
+        self.hist[player].append({'move':move, 'loc':self.pos[player]})
+        return
+
+    def calculate(self, player, decision):
+        return [self.pos[player][0] +  self.translate[decision][0], \
+                self.pos[player][1] +  self.translate[decision][1]]
+
+    def checkBoundry(self, player, decision):
+        new_pos = self.calculate(player, decision)
+        if 0 < new_pos[0] and self.axis[0] > new_pos[0]:
+            if 0 < new_pos[1] and self.axis[1] > new_pos[1]:
+                return new_pos
+        return False
+
+    def decisionAi(self):
+        new_pos = None
+        choices = {'w':0, 'a':0, 's':0, 'd':0}
+        old_dist = self.distance()
+        new_dist = self.distance()
+        for decision in choices:
+            new_pos = self.checkBoundry('ai', decision)
+            if new_pos:
+                choices[decision] = self.distance(ai_pos=new_pos)
+        new_pos = self.checkBoundry('ai', max(choices, key=choices.get))
+        self.pos['ai'] = new_pos
+        self.appendHistory('ai', new_pos)
+        return
+
+    def decisionUsr(self, window):
+        window.addstr(1, 1, "Which direction (w, a, s, d): ")
+        decision = window.getkey(1,32)
+        return
+
+    def distance(self, ai_pos=None, usr_pos=None):
+        if None == ai_pos: ai_pos = self.pos['ai']
+        if None == usr_pos: usr_pos = self.pos['usr']
+        a = math.pow(ai_pos[0] - usr_pos[0], 2)
+        b = math.pow(ai_pos[1] - usr_pos[1], 2)
+        return math.sqrt(a + b)
+
+    def placePlayers(self):
+        while self.distance() < (self.axis[0] / 3):
+            self.pos['ai'] = [random.randint(0, self.axis[0]), random.randint(0, self.axis[1])]
+            self.pos['usr'] = [random.randint(0, self.axis[0]), random.randint(0, self.axis[1])]
+        self.appendHistory('ai', ' ')
+        self.appendHistory('usr', ' ')
+        return
+
     def run(self):
+        usr_win = self.window.subwin(8, 50, 1, 1)
+        while not self.quit:
+            self.decisionAi()
+            self.decisionUsr(usr_win)
+            self.window.addstr(20, 1, "{}".format(self.pos))
+            self.window.refresh()
         return
 
     def start(self, winscr):
@@ -52,7 +125,7 @@ class TheHunt:
 
         with open(LEVELSFILE) as lvls:
             settings = json.load(lvls)
-            settings = settings['levels'][int(level)]
+            settings = settings['levels'][int(level) - 1]
         
         self.axis = settings['axis']
         self.grid = settings['grid']
@@ -64,4 +137,7 @@ class TheHunt:
         start_win.addstr(13, 14, "{} Let the Hunt begin!".format(level))
         start_win.refresh()
         sleep(1)
+        start_win.erase()
+        start_win.refresh()
+        self.placePlayers()
         self.run()
